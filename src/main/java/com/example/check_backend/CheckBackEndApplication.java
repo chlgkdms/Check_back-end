1121a4d726175e9e24d9d91e1daca8e8036f4a0a
chlgkdms <haeun3882@gmail.com>
2024-04-03 23:03:19 +0900

    setting :: 프로젝트 세팅

---
+++ src/main/java/com/example/check_backend/CheckBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.check_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/check_backend/CheckBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.check_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
